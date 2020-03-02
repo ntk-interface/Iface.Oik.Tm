82ac0a98735bb7219277ae24b3fb4fcb9b5ae299
alexeid <alexeid@iface.ru>
2020-03-02 15:00:08 +0500

    проверяю сборку 32-бит

--- src/Iface.Oik.Tm.Native/Api/TmNative.DllImport.cs
+++ src/Iface.Oik.Tm.Native/Api/TmNative.DllImport.cs
@@ -2,7 +2,12 @@ namespace Iface.Oik.Tm.Native.Api
 {
   public partial class TmNative
   {
+#if x86
     private const string Cfshare = "cfshare.dll";
     private const string Tmconn  = "tmconn.dll";
+#else
+    private const string Cfshare = "libif_cfs";
+    private const string Tmconn  = "libif_cfs";
+#endif
   }
 }
