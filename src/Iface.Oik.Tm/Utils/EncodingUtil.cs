c7e13531d4398a7137db399d9c27de142d8161ae
Evgeniy Kormiltsev <goodw@protonmail.com>
2020-03-20 15:14:23 +0500

    Дописал метод смены кодировки байтов и модифицировал метод конвертации строки Win1251 -> UTF-8

--- src/Iface.Oik.Tm/Utils/EncodingUtil.cs
+++ src/Iface.Oik.Tm/Utils/EncodingUtil.cs
@@ -17,12 +17,22 @@ namespace Iface.Oik.Tm.Utils
     public static string Win1251ToUtf8(string src)
     {
       if (src == null) return null;
-
-      var utf8    = Encoding.UTF8;
+      
       var win1251 = Encoding.GetEncoding(1251);
-      return utf8.GetString(Encoding.Convert(win1251, utf8, win1251.GetBytes(src)));
+      return Win1251BytesToUft8(win1251.GetBytes(src));
     }
+    
 
+    public static string Win1251BytesToUft8(byte[] src)
+    {
+      if (src == null) return null;
+      
+      var utf8   = Encoding.UTF8;
+      var cp1251 = Encoding.GetEncoding(1251);
+      return utf8.GetString(Encoding.Convert(cp1251, utf8, src));
+    }
+    
+    
     public static string Cp866ToUtf8(string src)
     {
       if (src == null) return null;
@@ -30,8 +40,8 @@ namespace Iface.Oik.Tm.Utils
       var cp866 = Encoding.GetEncoding(866);
       return Cp866BytesToUtf8String(cp866.GetBytes(src));
     }
-    
-    
+
+
     public static string Cp866BytesToUtf8String(byte[] src)
     {
       if (src == null) return null;
