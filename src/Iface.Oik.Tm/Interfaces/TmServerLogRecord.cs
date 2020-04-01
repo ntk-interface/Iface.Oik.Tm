e1a7970a30bdd6d572b8651230c8e6e629749cf0
Evgeniy Kormiltsev <goodw20@gmail.com>
2020-04-01 18:01:54 +0500

    Добавил класс записи журнала сервера

--- src/Iface.Oik.Tm/Interfaces/TmServerLogRecord.cs
+++ src/Iface.Oik.Tm/Interfaces/TmServerLogRecord.cs
@@ -1,33 +1,42 @@
 using System;
+using Iface.Oik.Tm.Native.Interfaces;
 using Iface.Oik.Tm.Utils;
 
 namespace Iface.Oik.Tm.Interfaces
 {
   public class TmServerLogRecord
   {
-    public string                Message    { get; }
-    public TmServerLogRecordType RecordType { get; }
-    public string                Source     { get; }
-    public int                   ThreadId   { get; }
-    public DateTime?             DateTime   { get; }
+    private int _hashCode { get; }
+    
+    
+    public string                Message    { get; private set; }
+    public TmServerLogRecordType RecordType { get; private set; }
+    public string                SourceServer     { get; private set; }
+    public int                   ThreadId   { get; private set; }
+    public DateTime?             DateTime   { get; private set; }
 
     public string Date => DateTime.HasValue ? DateTime.Value.ToString("dd.MM.yyyy") : "";
     public string Time => DateTime.HasValue ? DateTime.Value.ToString("HH:mm:ss.fff") : "";
-    
 
 
-    public TmServerLogRecord(string message,
-                             string recordType,
-                             string source,
-                             string threadId,
-                             string date,
-                             string time)
+
+    public TmServerLogRecord(int hashCode)
     {
-      Message    = message;
-      RecordType = ParseRecordType(recordType);
-      Source     = source;
-      ThreadId   = Convert.ToInt32(threadId, 16);
-      DateTime   = DateUtil.GetDateTime($"{date} {time}");
+      _hashCode = hashCode;
+    }
+
+
+    public static TmServerLogRecord CreateFromCfsLogRecord(TmNativeDefs.CfsLogRecord cfsLogRecord)
+    {
+      return new TmServerLogRecord((cfsLogRecord.Date, cfsLogRecord.Time, cfsLogRecord.Message).ToTuple().GetHashCode())
+             {
+               Message      = cfsLogRecord.Message,
+               RecordType   = ParseRecordType(cfsLogRecord.MsgType),
+               SourceServer = cfsLogRecord.Type,
+               ThreadId     = Convert.ToInt32(cfsLogRecord.ThreadId, 16),
+               DateTime     = DateUtil.GetDateTime($"{cfsLogRecord.Date} {cfsLogRecord.Time}")
+             };
+
     }
 
 
