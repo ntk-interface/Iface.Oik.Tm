d05587ccba970ea89db4897742783eac031b2882
Evgeniy Kormiltsev <goodw@protonmail.com>
2020-04-07 15:51:17 +0500

    Добавил override GetHashCode, поменял источник SourceServer, добавил метод получения строки RecordType

--- src/Iface.Oik.Tm/Interfaces/TmServerLogRecord.cs
+++ src/Iface.Oik.Tm/Interfaces/TmServerLogRecord.cs
@@ -6,18 +6,18 @@ namespace Iface.Oik.Tm.Interfaces
 {
   public class TmServerLogRecord
   {
-    private int _hashCode { get; }
-    
-    
-    public string                Message    { get; private set; }
-    public TmServerLogRecordType RecordType { get; private set; }
-    public string                SourceServer     { get; private set; }
-    public int                   ThreadId   { get; private set; }
-    public DateTime?             DateTime   { get; private set; }
+    private readonly int _hashCode;
 
+
+    public string                Message      { get; private set; }
+    public TmServerLogRecordTypes RecordTypes   { get; private set; }
+    public string                SourceServer { get; private set; }
+    public int                   ThreadId     { get; private set; }
+    public DateTime?             DateTime     { get; private set; }
+
+    public string RecordTypeString => RecordTypes.GetDescription();
     public string Date => DateTime.HasValue ? DateTime.Value.ToString("dd.MM.yyyy") : "";
     public string Time => DateTime.HasValue ? DateTime.Value.ToString("HH:mm:ss.fff") : "";
-
 
 
     public TmServerLogRecord(int hashCode)
@@ -31,26 +31,30 @@ namespace Iface.Oik.Tm.Interfaces
       return new TmServerLogRecord((cfsLogRecord.Date, cfsLogRecord.Time, cfsLogRecord.Message).ToTuple().GetHashCode())
              {
                Message      = cfsLogRecord.Message,
-               RecordType   = ParseRecordType(cfsLogRecord.MsgType),
-               SourceServer = cfsLogRecord.Type,
+               RecordTypes   = ParseRecordType(cfsLogRecord.MsgType),
+               SourceServer = cfsLogRecord.Name,
                ThreadId     = Convert.ToInt32(cfsLogRecord.ThreadId, 16),
                DateTime     = DateUtil.GetDateTime($"{cfsLogRecord.Date} {cfsLogRecord.Time}")
              };
-
     }
 
 
-    private static TmServerLogRecordType ParseRecordType(string recordTypeString)
+    private static TmServerLogRecordTypes ParseRecordType(string recordTypeString)
     {
       switch (recordTypeString)
       {
         case "MSG":
-          return TmServerLogRecordType.Msg;
+          return TmServerLogRecordTypes.Msg;
         case "ERROR":
-          return TmServerLogRecordType.Error;
+          return TmServerLogRecordTypes.Error;
         default:
-          return TmServerLogRecordType.Undefined;
+          return TmServerLogRecordTypes.Undefined;
       }
+    }
+
+    public override int GetHashCode()
+    {
+      return _hashCode;
     }
   }
 }
