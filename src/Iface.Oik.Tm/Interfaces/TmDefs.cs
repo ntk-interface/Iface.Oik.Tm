04b93ebfc498a07c07ff64028974cf813b2401fe
alexeid <alexeid@iface.ru>
2022-10-12 21:29:21 +0500

    переименование права доступа настроек АРМ

--- src/Iface.Oik.Tm/Interfaces/TmDefs.cs
+++ src/Iface.Oik.Tm/Interfaces/TmDefs.cs
@@ -212,7 +212,7 @@ namespace Iface.Oik.Tm.Interfaces
     ReadAllSchemeCatalogs       = 48,
     Ack                         = 49,
     OverrideControlScript       = 50,
-    EditGlobalSettings          = 53,
+    EditGlobalClientSettings    = 53,
     NotExists                   = 1000,
   }
 
