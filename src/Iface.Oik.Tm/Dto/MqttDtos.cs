6cd06e955c94d6c1ff1446e790a5c114c84e8df4
alexeid <alexeid@iface.ru>
2023-03-15 08:35:34 +0500

    работа над MQTT

--- src/Iface.Oik.Tm/Dto/MqttDtos.cs
+++ src/Iface.Oik.Tm/Dto/MqttDtos.cs
@@ -13,18 +13,19 @@ namespace Iface.Oik.Tm.Dto
 
   public class MqttTopologyChangeSomeDto
   {
-     public Dictionary<Guid, CimTopologyStatus> Items { get; set; }
+    public Dictionary<Guid, CimTopologyStatus> Items { get; set; }
   }
-  
-  
+
+
   public class MqttUserActionLogDto
   {
-    public Guid               Id         { get; set; }
-    public DateTime           Time       { get; set; }
-    public MqttKnownTopic     Action     { get; set; }
-    public TmEventImportances Importance { get; set; }
-    public string             Text       { get; set; }
-    public string             UserName   { get; set; }
+    public Guid           Id         { get; set; }
+    public DateTime       Time       { get; set; }
+    public MqttKnownTopic Action     { get; set; }
+    public string         ActionName { get; set; }
+    public int            Importance { get; set; }
+    public string         Text       { get; set; }
+    public string         Username   { get; set; }
   }
 
 
@@ -32,5 +33,47 @@ namespace Iface.Oik.Tm.Dto
   {
     public int    Ownership { get; set; }
     public string Name      { get; set; }
+  }
+
+
+  public class MqttPlacardAddedDto
+  {
+    public Guid   Id          { get; set; }
+    public int    TypeId      { get; set; }
+    public string TypeName    { get; set; }
+    public Guid   EquipmentId { get; set; }
+    public int?   Index       { get; set; }
+    public string Comment     { get; set; }
+  }
+
+
+  public class MqttPlacardEditedDto
+  {
+    public Guid     Id          { get; set; }
+    public int      TypeId      { get; set; }
+    public string   TypeName    { get; set; }
+    public Guid     EquipmentId { get; set; }
+    public DateTime CreatedTime { get; set; }
+    public int?     Index       { get; set; }
+    public string   Comment     { get; set; }
+    public int?     NewIndex    { get; set; }
+    public string   NewComment  { get; set; }
+  }
+
+
+  public class MqttPlacardRemovedDto
+  {
+    public Guid     Id          { get; set; }
+    public int      TypeId      { get; set; }
+    public string   TypeName    { get; set; }
+    public Guid     EquipmentId { get; set; }
+    public DateTime CreatedTime { get; set; }
+  }
+
+
+  public class MqttPermittedPortableGroundDto
+  {
+    public Guid EquipmentId { get; set; }
+    public bool IsPermitted { get; set; }
   }
 }
