72f7023ad6a8bc6d1aa0c8454456e6242c14e65e
Евгений Кормильцев(младший) <eugene_jr@iface.ru>
2024-12-18 14:02:17 +0500

    Переделал логику смены ролей и добавил отдельные методы для dry run и непосредственной смены ролей

--- src/Iface.Oik.Tm/Interfaces/ICfsApi.cs
+++ src/Iface.Oik.Tm/Interfaces/ICfsApi.cs
@@ -7,177 +7,234 @@ using System.Threading.Tasks;
 
 namespace Iface.Oik.Tm.Interfaces
 {
-	public interface ICfsApi
-	{
-		void SetCfIdAndHost(IntPtr cfId, string host);
-		string MakeInprocCrd(string machine, string user, string pwd);
-		Task<(IntPtr, DateTime)> OpenConfigurationTree(string fileName);
-		IntPtr CreateConfigurationTree(IEnumerable<CfTreeNode> tree);
-		Task<(IntPtr, DateTime)> OpenMasterServiceConfiguration();
+  public interface ICfsApi
+  {
+    void                     SetCfIdAndHost(IntPtr                           cfId,    string host);
+    string                   MakeInprocCrd(string                            machine, string user, string pwd);
+    Task<(IntPtr, DateTime)> OpenConfigurationTree(string                    fileName);
+    IntPtr                   CreateConfigurationTree(IEnumerable<CfTreeNode> tree);
+    Task<(IntPtr, DateTime)> OpenMasterServiceConfiguration();
 
-		Task<IReadOnlyCollection<CfTreeNode>> GetReserveConfiguration();
+    Task<IReadOnlyCollection<CfTreeNode>> GetReserveConfiguration();
 
-		Task<ReserveServerState> ReserveServerTypeIsWorking(CfTreeNode reserveConfNode);
+    Task<ReserveServerState> ReserveServerTypeIsWorking(CfTreeNode reserveConfNode);
 
-		Task OverrideReservePipe(string pipeName, BroadcastServerSignature signature);
-
-		
-		Task<(MSTreeNode, DateTime)> LoadFullMSTree();
-		Task SaveConfigurationTree(IntPtr treeHandle, string filename);
-		Task SaveMasterServiceConfiguration(IntPtr treeHandle);
-		Task SaveFullMSTree(MSTreeNode msRoot);
-		Task<List<CfTreeNode>> GetCfTree(IntPtr rootHandle, CfTreeNode parent = null);
-		void FreeConfigurationTreeHandle(IntPtr handle);
-		void FreeMasterServiceConfigurationHandle(IntPtr handle);
+    Task OverrideReservePipe(string pipeName, BroadcastServerSignature signature);
 
 
-		Task<CfsDefs.SoftwareTypes> GetSoftwareType();
+    Task<(MSTreeNode, DateTime)> LoadFullMSTree();
+    Task                         SaveConfigurationTree(IntPtr                treeHandle, string filename);
+    Task                         SaveMasterServiceConfiguration(IntPtr       treeHandle);
+    Task                         SaveFullMSTree(MSTreeNode                   msRoot);
+    Task<List<CfTreeNode>>       GetCfTree(IntPtr                            rootHandle, CfTreeNode parent = null);
+    void                         FreeConfigurationTreeHandle(IntPtr          handle);
+    void                         FreeMasterServiceConfigurationHandle(IntPtr handle);
 
 
-		Task<CfsDefs.MasterServiceStatus> MasterServiceStatus();
+    Task<CfsDefs.SoftwareTypes> GetSoftwareType();
 
 
-		Task StartMasterService();
+    Task<CfsDefs.MasterServiceStatus> MasterServiceStatus();
 
 
-		Task StopMasterService();
+    Task StartMasterService();
 
 
-		Task<bool> IsConnected();
-
-		Task<string> GetBasePath();
-
-		Task<IReadOnlyCollection<string>> GetTimezones();
-
-		Task<IReadOnlyCollection<TmServer>> GetTmServersTree();
+    Task StopMasterService();
 
 
-		Task<IReadOnlyCollection<TmServerLogRecord>> GetTmServersLog();
+    Task<bool> IsConnected();
+
+    Task<string> GetBasePath();
+
+    Task<IReadOnlyCollection<string>> GetTimezones();
+
+    Task<IReadOnlyCollection<TmServer>> GetTmServersTree();
 
 
-		Task<IReadOnlyCollection<TmServerLogRecord>> GetTmServersLog(int maxRecords, DateTime? startTime, DateTime? endTime);
+    Task<IReadOnlyCollection<TmServerLogRecord>> GetTmServersLog();
 
 
-		Task<IReadOnlyCollection<TmServerThread>> GetTmServersThreads();
+    Task<IReadOnlyCollection<TmServerLogRecord>>
+      GetTmServersLog(int maxRecords, DateTime? startTime, DateTime? endTime);
 
 
-		Task RegisterTmServerTracer(ITmServerTraceable traceTarget, bool debug, int pause);
+    Task<IReadOnlyCollection<TmServerThread>> GetTmServersThreads();
 
 
-		Task StopTmServerTrace();
+    Task RegisterTmServerTracer(ITmServerTraceable traceTarget, bool debug, int pause);
 
 
-		Task<IReadOnlyCollection<TmServerLogRecord>> TraceTmServerLogRecords();
+    Task StopTmServerTrace();
 
 
-		Task<TmInstallationInfo> GetTmInstallationInfo();
+    Task<IReadOnlyCollection<TmServerLogRecord>> TraceTmServerLogRecords();
 
 
-		Task<TmLicenseInfo> GetLicenseInfo();
+    Task<TmInstallationInfo> GetTmInstallationInfo();
 
 
-		Task<IReadOnlyCollection<LicenseKeyType>> GetAvailableLicenseKeyTypes();
+    Task<TmLicenseInfo> GetLicenseInfo();
 
 
-		Task SetLicenseKeyCom(TmLicenseKey newLicenseKey);
+    Task<IReadOnlyCollection<LicenseKeyType>> GetAvailableLicenseKeyTypes();
 
 
-		Task<IReadOnlyCollection<string>> GetFilesInDirectory(string path);
+    Task SetLicenseKeyCom(TmLicenseKey newLicenseKey);
 
 
-		Task<(bool, string)> PutFile(string localFilePath,
-					 string remoteFilePath,
-					 uint timeout);
-
-		Task DeleteFile(string remoteFilePath);
+    Task<IReadOnlyCollection<string>> GetFilesInDirectory(string path);
 
 
-		Task<IReadOnlyCollection<string>> GetInstalledLicenseKeyFiles();
+    Task<(bool, string)> PutFile(string localFilePath,
+                                 string remoteFilePath,
+                                 uint   timeout);
+
+    Task DeleteFile(string remoteFilePath);
 
 
-		Task PutLicenseKeyFile(string filePath);
+    Task<IReadOnlyCollection<string>> GetInstalledLicenseKeyFiles();
 
 
-		Task DeleteLicenseKeyFile(string fileName);
+    Task PutLicenseKeyFile(string filePath);
 
 
-		Task<IReadOnlyCollection<SLogRecord>> GetSecurityLogFull(SLogReadDirection readDirection = SLogReadDirection.FromEnd);
+    Task DeleteLicenseKeyFile(string fileName);
 
 
-		Task<IReadOnlyCollection<SLogRecord>> GetAdministratorLogFull(SLogReadDirection readDirection = SLogReadDirection.FromEnd);
+    Task<IReadOnlyCollection<SLogRecord>> GetSecurityLogFull(
+      SLogReadDirection readDirection = SLogReadDirection.FromEnd);
 
 
-		Task<IReadOnlyCollection<SLogRecord>> GetSecurityLog(int               maxRecords,
-		                                                     SLogReadDirection readDirection = SLogReadDirection.FromEnd, 
-		                                                     uint              startIndex    = SLogIndex.Last,
-		                                                     DateTime?         startTime     = null, 
-		                                                     DateTime?         endTime       = null);
+    Task<IReadOnlyCollection<SLogRecord>> GetAdministratorLogFull(
+      SLogReadDirection readDirection = SLogReadDirection.FromEnd);
 
 
-		Task<IReadOnlyCollection<SLogRecord>> GetAdministratorLog(int maxRecords,
-		                                                          SLogReadDirection readDirection = SLogReadDirection.FromEnd, 
-		                                                          uint              startIndex = SLogIndex.Last,
-		                                                          DateTime?         startTime = null, 
-		                                                          DateTime?         endTime = null);
+    Task<IReadOnlyCollection<SLogRecord>> GetSecurityLog(int               maxRecords,
+                                                         SLogReadDirection readDirection = SLogReadDirection.FromEnd,
+                                                         uint              startIndex    = SLogIndex.Last,
+                                                         DateTime?         startTime     = null,
+                                                         DateTime?         endTime       = null);
 
 
-		Task<int> GetRedirectorPort(string pipeName, int portIndex);
+    Task<IReadOnlyCollection<SLogRecord>> GetAdministratorLog(int maxRecords,
+                                                              SLogReadDirection readDirection =
+                                                                SLogReadDirection.FromEnd,
+                                                              uint      startIndex = SLogIndex.Last,
+                                                              DateTime? startTime  = null,
+                                                              DateTime? endTime    = null);
 
 
-		Task<byte[]> GetBin(string uName,
-		                    string oName,
-		                    string binName);
-
-		Task<(byte[], uint, string)> SecGetBin(string uName,
-												 string oName,
-												 string binName);
-		Task<bool> SetRedirectorPort(string pipeName, int portIndex, int port);
+    Task<int> GetRedirectorPort(string pipeName, int portIndex);
 
 
-		Task<bool> SetBin(string uName,
-		                  string oName,
-		                  string binName,
-		                  byte[] binData);
-		Task<(uint, string)> SecSetBin(string uName,
-											   string oName,
-											   string binName,
-											   byte[] binData);
-		AccessMasksDescriptor SecGetAccessDescriptor(string ProgName);
-		ExtendedRightsDescriptor SecGetExtendedRightsDescriptor();
-		Task<(IReadOnlyCollection<string>, uint, string)> SecEnumUsers();
-		Task<(IReadOnlyCollection<string>, uint, string)> SecEnumOSUsers();
-		Task<(uint, string)> SecChangeUserPassword(string username, string password);
-		Task<(uint, string)> SecDeleteUser(string username);
-		Task<(uint, uint, string)> SecGetAccessMask(string uName, string oName);
-		Task<(uint, string)> SecSetAccessMask(string uName, string oName, uint AccessMask);
-		Task<(ExtendedUserData, uint, string)> SecGetExtendedUserData(string serverType, string serverName, string username);
-		Task<(uint, string)> SecSetExtendedUserData(string serverType, string serverName, string username, ExtendedUserData extendedUserData);
-		Task<(UserPolicy, uint, string)> SecGetUserPolicy(string username);
-		Task<(uint, string)> SecSetUserPolicy(string username, UserPolicy userPolicy);
-		Task<(PasswordPolicy, uint, string)> SecGetPasswordPolicy();
-		Task<(uint, string)> SecSetPasswordPolicy(PasswordPolicy passwordPolicy);
-		Task<(IReadOnlyCollection<string>, uint, string)> DirEnum(string Path);
-		Task<(bool, string)> SaveMachineConfig(string directory, bool full);
-		Task<(bool, string)> SaveMachineConfigEx(string directory, uint scope,
-																	  TmNativeCallback callback = null,
-																	  IntPtr callbackParameter = default);
-		Task<(bool, string)> RestoreMachineConfig(string filename);
-		Task<(bool, string)> CreateBackup(string progName, string pipeName, string directory, bool withRetro,
-								TmNativeCallback callback = null,
-								IntPtr callbackParameter = default);
-		Task<(bool, string)> RestoreBackup(string progName, string pipeName, string filename, bool withRetro,
-								 TmNativeCallback callback = null,
-								 IntPtr callbackParameter = default);
-		Task<(uint, string)> BackupSecurity(string directory, string pwd = "");
-		Task<(uint, string)> RestoreSecurity(string filename, string pwd);
-		Task<(IReadOnlyCollection<string>, string)> EnumPackedFiles(string pkfname);
-		Task<(IReadOnlyCollection<string>, string)> UnPack(string pkfname, string dirname);
-		Task<(bool, string)> ExtractFile(string pkfname, string filename, string dirname);
-		Task<(uint, string, UInt64, UInt32)> StartTestTmcalc(string tmsname, string clcname, UInt32 test_way, UInt32 test_flags);
-		Task<(uint, string)> StopTestTmcalc(UInt64 handle, UInt32 pid);
-		Task<(bool, string)> PmonCheckProcess(string process_name_args);
-		Task<(bool, string)> PmonStopProcess(string process_name_args);
-		Task<(bool, string)> PmonRestartProcess(string process_name_args);
-		Task<(bool, string)> SwapFnSrvRole(string serverName, Boolean b_pre, string fns_name);
-	}
+    Task<byte[]> GetBin(string uName,
+                        string oName,
+                        string binName);
+
+    Task<(byte[], uint, string)> SecGetBin(string uName,
+                                           string oName,
+                                           string binName);
+
+    Task<bool> SetRedirectorPort(string pipeName, int portIndex, int port);
+
+
+    Task<bool> SetBin(string uName,
+                      string oName,
+                      string binName,
+                      byte[] binData);
+
+    Task<(uint, string)> SecSetBin(string uName,
+                                   string oName,
+                                   string binName,
+                                   byte[] binData);
+
+    AccessMasksDescriptor SecGetAccessDescriptor(string progName);
+
+    ExtendedRightsDescriptor SecGetExtendedRightsDescriptor();
+
+    Task<(IReadOnlyCollection<string>, uint, string)> SecEnumUsers();
+
+    Task<(IReadOnlyCollection<string>, uint, string)> SecEnumOSUsers();
+
+    Task<(uint, string)> SecChangeUserPassword(string username, string password);
+
+    Task<(uint, string)> SecDeleteUser(string username);
+
+    Task<(uint, uint, string)> SecGetAccessMask(string uName, string oName);
+
+    Task<(uint, string)> SecSetAccessMask(string uName, string oName, uint AccessMask);
+
+    Task<(ExtendedUserData, uint, string)>
+      SecGetExtendedUserData(string serverType, string serverName, string username);
+
+    Task<(uint, string)> SecSetExtendedUserData(string           serverType, string serverName, string username,
+                                                ExtendedUserData extendedUserData);
+
+    Task<(UserPolicy, uint, string)> SecGetUserPolicy(string username);
+
+    Task<(uint, string)> SecSetUserPolicy(string username, UserPolicy userPolicy);
+
+    Task<(PasswordPolicy, uint, string)> SecGetPasswordPolicy();
+
+    Task<(uint, string)> SecSetPasswordPolicy(PasswordPolicy passwordPolicy);
+
+    Task<(IReadOnlyCollection<string>, uint, string)> DirEnum(string path);
+
+    Task<(bool, string)> SaveMachineConfig(string directory, bool full);
+
+    Task<(bool, string)> SaveMachineConfigEx(string           directory,
+                                             uint             scope,
+                                             TmNativeCallback callback          = null,
+                                             IntPtr           callbackParameter = default);
+
+    Task<(bool, string)> RestoreMachineConfig(string filename);
+
+    Task<(bool, string)> CreateBackup(string           progName,
+                                      string           pipeName,
+                                      string           directory,
+                                      bool             withRetro,
+                                      TmNativeCallback callback          = null,
+                                      IntPtr           callbackParameter = default);
+
+    Task<(bool, string)> RestoreBackup(string           progName,
+                                       string           pipeName,
+                                       string           filename,
+                                       bool             withRetro,
+                                       TmNativeCallback callback          = null,
+                                       IntPtr           callbackParameter = default);
+
+    Task<(uint, string)> BackupSecurity(string directory, string pwd = "");
+
+    Task<(uint, string)> RestoreSecurity(string filename, string pwd);
+
+    Task<(IReadOnlyCollection<string>, string)> EnumPackedFiles(string pkfName);
+
+    Task<(IReadOnlyCollection<string>, string)> UnPack(string pkfName,
+                                                       string dirname);
+
+    Task<(bool, string)> ExtractFile(string pkfName,
+                                     string filename,
+                                     string dirname);
+
+    Task<(uint, string, UInt64, UInt32)> StartTestTmcalc(string tmsName,
+                                                         string clcName,
+                                                         UInt32 testWay,
+                                                         UInt32 testFlags);
+
+    Task<(uint, string)> StopTestTmcalc(UInt64 handle,
+                                        UInt32 pid);
+
+    Task<(bool, string)> PmonCheckProcess(string processNameArgs);
+
+    Task<(bool, string)> PmonStopProcess(string processNameArgs);
+
+    Task<(bool, string)> PmonRestartProcess(string processNameArgs);
+
+    Task<(bool, string)> SwapFnSrvRole(string encodedCredentials, string fnsName, bool dryRun);
+
+    Task<(bool, string)> ReserveRoleSwapIsAvailable(string encodedCredentials, string pipeName);
+
+    Task<(bool, string)> SwapReserveRole(string encodedCredentials, string pipeName);
+  }
 }
