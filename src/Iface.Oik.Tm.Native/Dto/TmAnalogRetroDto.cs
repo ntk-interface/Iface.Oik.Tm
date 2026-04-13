9ab7e88648d4b2ab49b5b649cea47a23fe40a43d
Евгений Кормильцев(младший) <eugene_jr@iface.ru>
2026-04-13 10:20:16 +0500

    Убрал лишнего из TmAnalogRetroDto

--- src/Iface.Oik.Tm.Native/Dto/TmAnalogRetroDto.cs
+++ src/Iface.Oik.Tm.Native/Dto/TmAnalogRetroDto.cs
@@ -6,7 +6,6 @@ public record TmAnalogRetroDto
 {
   public float Value { get; init; }
   public int?  Code  { get; init; }
-  public float Load  { get; init; }
   public short Flags { get; init; }
   public long  Time  { get; init; }
 }
