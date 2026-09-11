8ce5fde5829c1a9f393f6a8686a8a7249f1be9b4
alexeid <alexeid@iface.ru>
2026-09-11 14:21:08 +0500

    TmUserActionCategory -> Operj

--- src/Iface.Oik.Tm/Interfaces/TmUserActionCategory.cs
+++ src/Iface.Oik.Tm/Interfaces/TmUserActionCategory.cs
@@ -5,14 +5,15 @@ namespace Iface.Oik.Tm.Interfaces
   // Не менять порядок ни в коем случае!
   public enum TmUserActionCategory
   {
-    [Description("")]                   None,
-    [Description("Аудит")]              Audit,
-    [Description("Каталог документов")] Documents,
-    [Description("Плакат")]             Placards,
-    [Description("Привязка")]           Bindings,
-    [Description("Настройки")]          Settings,
-    [Description("Сообщение")]          Message,
-    [Description("Телеметрия")]         Tm,
-    [Description("События")]            Events,
+    [Description("")]                    None,
+    [Description("Аудит")]               Audit,
+    [Description("Каталог документов")]  Documents,
+    [Description("Плакат")]              Placards,
+    [Description("Привязка")]            Bindings,
+    [Description("Настройки")]           Settings,
+    [Description("Сообщение")]           Message,
+    [Description("Телеметрия")]          Tm,
+    [Description("События")]             Events,
+    [Description("Оперативные журналы")] Operj,
   }
 }
